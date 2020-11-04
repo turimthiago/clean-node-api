072812ef83143aa9b49c3bbb02ea78d8d711eefb
Thiago Turim <thiagoturim@Thiagos-MacBook-Pro.local>
2020-11-04 00:21:38 -0300

    chore: fix jest/eslint updated

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,13 @@
 export default {
-  roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  coverageDirectory: 'coverage',
-  testEnvironment: 'node',
-  transform: { '.+\\.ts$': 'ts-jest' }
+  roots: ["<rootDir>/src"],
+  collectCoverageFrom: [
+    "<rootDir>/src/**/*.ts",
+    "!<rootDir>/src/main/**",
+    "!<rootDir>/src/**/*-protocols.ts",
+    "!**/protocols/**",
+    "!**/test/**"
+  ],
+  coverageDirectory: "coverage",
+  testEnvironment: "node",
+  transform: { ".+\\.ts$": "ts-jest" }
 }
