5018ae07a62e61e1536f91366b69cf971d329be5
Thiago Turim <thiagoturim@Thiagos-MacBook-Pro.local>
2020-11-08 15:14:19 -0300

    feat: ensure SighUpController return 400 if an invalid email

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,17 @@
+import { InvalidParamError } from "../errors/invalid-param-error";
 import { MissingParamError } from "../errors/missing-params-error";
 import { badRequest } from "../helpers/http-helpers";
 import { Controller } from "../protocols/controller";
+import { EmailValidator } from "../protocols/email-validator";
 import { HttpRequest, HttpResponse } from "../protocols/http";
 
 export class SignUpController implements Controller {
+  private readonly emailValidator: EmailValidator;
+
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator;
+  }
+
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = [
       "name",
@@ -15,6 +23,10 @@ export class SignUpController implements Controller {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field));
       }
+    }
+    const isValid = this.emailValidator.isValid(httpRequest.body.email);
+    if (!isValid) {
+      return badRequest(new InvalidParamError("email"));
     }
   }
 }
