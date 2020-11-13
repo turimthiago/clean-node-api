e6dfb9a8fe52ecbc0fa044cd4694a8cece2cfcd5
Thiago Turim <thiagoturim@Thiagos-MacBook-Pro.local>
2020-11-12 23:24:40 -0300

    feat: ensure EmailValidatorAdapter returns false if validator returns false

--- jest-unit-config.js
+++ jest-unit-config.js
@@ -1,4 +1,4 @@
 const { REPL_MODE_SLOPPY } = require("repl");
-const config = require("./jest-config");
-config.testMathc = ["**/*.spec.ts"];
+const config = require("./jest.config");
+config.testMatch = ["**/*.spec.ts"];
 module.exports = config;
