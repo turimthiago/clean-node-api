782bae21a11c8361e116d952d564a8db9e777454
Thiago Turim <thiagoturim@Thiagos-MacBook-Pro.local>
2020-11-26 23:43:30 -0300

    feat: ensure AccountMongoRepository returns account on sucess

---
+++ src/infra/db/mongodb/account-repository/helpers/mongo-helper.ts
@@ -0,0 +1,19 @@
+import { connect } from "http2";
+import { Collection, MongoClient } from "mongodb";
+import { disconnect } from "process";
+
+export const MongoHelper = {
+  client: null as MongoClient,
+  async connect (uri: string): Promise<void> {
+    this.client = await MongoClient.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+  },
+  async disconnect (): Promise<void> {
+    await this.client.close();
+  },
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name);
+  }
+};
