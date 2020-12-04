694078360eab3150eda99b00d4c36c2af15fdb29
Thiago Turim <thiagoturim@Thiagos-MacBook-Pro.local>
2020-12-04 01:58:15 -0300

    fix: fix ExpressAdapter return null

--- src/main/adapters/express-routes-adapter.ts
+++ src/main/adapters/express-routes-adapter.ts
@@ -1,12 +1,13 @@
 import { Request, Response } from "express";
-import { Controller, HttpRequest } from "../../presentation/protocols";
+import { Controller, HttpRequest, HttpResponse } from "../../presentation/protocols";
 
 export const adaptRoute = (controller: Controller) => {
   return async (request: Request, response: Response) => {
     const httpRequest: HttpRequest = {
       body: request.body
     };
-    const httpResponse = await controller.handle(httpRequest);
+    const httpResponse: HttpResponse = await controller.handle(httpRequest);
+    console.log(httpResponse);
     response.status(httpResponse.statusCode).json(httpResponse.body);
   };
 };
--- src/main/decorators/log.ts
+++ src/main/decorators/log.ts
@@ -14,7 +14,6 @@ export class LogControllerDecorator implements Controller {
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.controller.handle(httpRequest);
-    return null;
+    return await this.controller.handle(httpRequest);
   }
 }
