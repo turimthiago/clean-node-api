4549460289164f8ac8f51202221226749827c39d
Thiago Turim <thiagoturim@Thiagos-MacBook-Pro.local>
2020-12-04 01:27:18 -0300

    feat: ensure LogControllerDecorator calls Controller with correct value

---
+++ src/main/decorators/log.ts
@@ -0,0 +1,20 @@
+import { response } from "express";
+import { resolve } from "path";
+import {
+  Controller,
+  HttpRequest,
+  HttpResponse
+} from "../../presentation/protocols";
+
+export class LogControllerDecorator implements Controller {
+  private readonly controller: Controller;
+
+  constructor (controller: Controller) {
+    this.controller = controller;
+  }
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    await this.controller.handle(httpRequest);
+    return null;
+  }
+}
