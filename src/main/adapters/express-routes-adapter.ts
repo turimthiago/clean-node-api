711a710b5d66a30460932409e7f3e97cf3ba480a
Thiago Turim <thiagoturim@Thiagos-MacBook-Pro.local>
2020-12-06 14:23:49 -0300

    refactor: maker server error return the error stack

--- src/main/adapters/express-routes-adapter.ts
+++ src/main/adapters/express-routes-adapter.ts
@@ -1,12 +1,16 @@
 import { Request, Response } from "express";
-import { Controller, HttpRequest, HttpResponse } from "../../presentation/protocols";
+import {
+  Controller,
+  HttpRequest,
+  HttpResponse
+} from "../../presentation/protocols";
 
 export const adaptRoute = (controller: Controller) => {
   return async (request: Request, response: Response) => {
     const httpRequest: HttpRequest = {
       body: request.body
     };
-    const httpResponse: HttpResponse = await controller.handle(httpRequest);
+    const httpResponse = await controller.handle(httpRequest);
     console.log(httpResponse);
     response.status(httpResponse.statusCode).json(httpResponse.body);
   };
