0b8655339757aaba55f7d4fe1fb5ee16d1194008
Thiago Turim <thiagoturim@Thiagos-MacBook-Pro.local>
2020-12-22 00:30:37 -0300

    test: ensure RequiredFieldValidation not return if validation success

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -7,4 +7,9 @@ describe("RequiredField Validation", () => {
     const error = sut.validate({ name: "any_name" });
     expect(error).toEqual(new MissingParamError("any_field"));
   });
+
+  test("Should not return if valitaion success", () => {
+    const sut = new RequiredFieldValidation("any_field");
+    expect(sut.validate({ any_field: "any_field_value" })).toBeFalsy();
+  });
 });
