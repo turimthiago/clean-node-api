63dee26b17ad8144e439aa7be39d2910151831a8
Thiago Turim <thiagoturim@Thiagos-MacBook-Pro.local>
2020-12-22 01:51:02 -0300

    refactor: remove is not null from SignUpController

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -19,7 +19,7 @@ export class SignUpController implements Controller {
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const error = this.validation?.validate(httpRequest.body);
+      const error = this.validation.validate(httpRequest.body);
       if (error) return badRequest(error);
       const { name, email, password } = httpRequest.body;
       const account = await this.addAccount.add({
