2edd2f1a3b6514233d01c2b54ef30424faedca77
Thiago Turim <thiagoturim@Thiagos-MacBook-Pro.local>
2020-12-26 08:13:34 -0300

    feat: ensure DbAuthentication returns a token on success

--- src/data/authentication/db-authentication.ts
+++ src/data/authentication/db-authentication.ts
@@ -27,8 +27,14 @@ export class DbAuthentication implements Authentication {
       authentication.email
     );
     if (account) {
-      await this.hashComparer.comparer(authentication.password, account.password);
-      await this.tokenGenerator.generate(account.id);
+      const isValid = await this.hashComparer.comparer(
+        authentication.password,
+        account.password
+      );
+      if (isValid) {
+        const accessToken = await this.tokenGenerator.generate(account.id);
+        return accessToken;
+      }
     }
     return null;
   }
