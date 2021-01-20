7e1699e134b7dd1bf44e38205ffa53a1dd974048
Thiago Turim <thiagoturim@Thiagos-MacBook-Pro.local>
2021-01-20 15:16:43 -0300

    feat: add login route

--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { adaptRoute } from "../adapters/express/express-routes-adapter";
+import { makeLoginController } from "../factories/login/login-factory";
 import { makeSignUpController } from "../factories/signup/signup-factory";
 
 export default async (router: Router): Promise<void> => {
   await router.post("/signup", adaptRoute(makeSignUpController()));
+  await router.post("/login", adaptRoute(makeLoginController()));
 };
