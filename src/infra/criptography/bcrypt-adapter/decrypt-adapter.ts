eb2170d10055bb1df006d000f37456faddd6c8f5
Thiago Turim Carvalho <turimthiago@gmail.com>
2021-03-03 23:31:24 -0300

    feat: ensure DbLoadAccountByToken call Dcrypter with correct values

---
+++ src/infra/criptography/bcrypt-adapter/decrypt-adapter.ts
@@ -0,0 +1,17 @@
+import bcrypt from "bcrypt";
+import { HashComparer } from "../../../data/protocols/criptography/hash-comparer";
+import { Hasher } from "../../../data/protocols/criptography/hasher";
+
+export class BcryptAdapter implements Hasher, HashComparer {
+  constructor (private readonly salt: number) {
+  }
+
+  async compare (value: string, hash: string): Promise<Boolean> {
+    const isEquals = await bcrypt.compare(value, hash);
+    return isEquals;
+  }
+
+  async hash (value: string): Promise<string> {
+    return await bcrypt.hash(value, this.salt);
+  }
+}
