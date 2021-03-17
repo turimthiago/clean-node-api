b980762d4d52d62e9c6bc559803303a96a1faab4
Thiago Turim Carvalho <turimthiago@gmail.com>
2021-03-16 23:28:48 -0300

    feat: ensure LoadSurveysController calls LoadSurveys usecase

---
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.spec.ts
@@ -0,0 +1,46 @@
+import {
+  LoadSurveys,
+  SurveyModel,
+  LoadSurveysController
+} from "./load-survey-protocols";
+import MockDate from "mockdate";
+
+const makeFackeSurveys = (): SurveyModel[] => {
+  return [
+    {
+      id: "any_id",
+      question: "any_question",
+      answers: [{ image: "any_image", answer: "any_answer" }],
+      date: new Date()
+    },
+    {
+      id: "other_id",
+      question: "other_question",
+      answers: [{ image: "other_image", answer: "other_answer" }],
+      date: new Date()
+    }
+  ];
+};
+
+describe("LoadSurveys Controller", () => {
+  beforeAll(() => {
+    MockDate.set(new Date());
+  });
+
+  afterAll(() => {
+    MockDate.reset();
+  });
+
+  test("Should call LoadSurveys ", async () => {
+    class LoadSurveysStub implements LoadSurveys {
+      async load (): Promise<SurveyModel[]> {
+        return await Promise.resolve(makeFackeSurveys());
+      }
+    }
+    const loadSurveysStub = new LoadSurveysStub();
+    const loadSpy = jest.spyOn(loadSurveysStub, "load");
+    const sut = new LoadSurveysController(loadSurveysStub);
+    await sut.handle({});
+    expect(loadSpy).toHaveBeenCalled();
+  });
+});
---
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -0,0 +1,15 @@
+import { LoadSurveys } from "../../../../domain/usecases/load-surveys";
+import {
+  Controller,
+  HttpRequest,
+  HttpResponse
+} from "../add-suvey/add-survey-protocols";
+
+export class LoadSurveysController implements Controller {
+  constructor (private readonly loadSurveys: LoadSurveys) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    await this.loadSurveys.load();
+    return null;
+  }
+}
