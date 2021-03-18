5cf8eddf5beba1ab7e272ac2202b4b7abe5ab915
Thiago Turim Carvalho <turimthiago@gmail.com>
2021-03-17 23:00:07 -0300

    feat: ensure SurveyMongoRepository load all surveys

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -2,11 +2,20 @@ import {
   AddSurveyModel,
   AddSurveyRepository
 } from "../../../../data/protocols/db/survey/add-survey-protocols";
+import { LoadSurveysRepository } from "../../../../data/protocols/db/survey/load-surveys-repository";
+import { SurveyModel } from "../../../../domain/models/survey";
 import { MongoHelper } from "../helpers/mongo-helper";
 
-export class SurveyMongoRepository implements AddSurveyRepository {
+export class SurveyMongoRepository
+implements AddSurveyRepository, LoadSurveysRepository {
   async add (data: AddSurveyModel): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection("surveys");
     await surveyCollection.insertOne(data);
+  }
+
+  async loadAll (): Promise<SurveyModel[]> {
+    const surveyCollection = await MongoHelper.getCollection("surveys");
+    const surveys = surveyCollection.find().toArray();
+    return await surveys;
   }
 }
