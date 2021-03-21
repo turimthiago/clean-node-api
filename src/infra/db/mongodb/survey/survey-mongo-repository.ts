af4c6b3799266cee4d717db73b1138972e231c16
Thiago Turim Carvalho <turimthiago@gmail.com>
2021-03-21 16:38:46 -0300

    feat: ensure SurveyMongoRepository find survey by id with success

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -5,9 +5,18 @@ import {
 import { LoadSurveysRepository } from "../../../../data/protocols/db/survey/load-surveys-repository";
 import { SurveyModel } from "@/domain/models/survey";
 import { MongoHelper } from "../helpers/mongo-helper";
+import { LoadSurveyByIdRepository } from "@/data/usecases/load-survey-by-id/db-load-load-survey-by-id-protocols";
 
 export class SurveyMongoRepository
-implements AddSurveyRepository, LoadSurveysRepository {
+implements
+    AddSurveyRepository,
+    LoadSurveysRepository,
+    LoadSurveyByIdRepository {
+  async loadById (id: string): Promise<SurveyModel> {
+    const surveyCollection = await MongoHelper.getCollection("surveys");
+    return await surveyCollection.findOne({ _id: id });
+  }
+
   async add (data: AddSurveyModel): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection("surveys");
     await surveyCollection.insertOne(data);
