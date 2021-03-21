f9025d769fc91711ed475542dfa4ef7d7228cd40
Thiago Turim Carvalho <turimthiago@gmail.com>
2021-03-21 15:46:14 -0300

    feat: ensure DbSaveSurveyResult returns a SurveyResult on success

--- src/data/usecases/db-save-survey-result/db-save-survey-result.ts
+++ src/data/usecases/db-save-survey-result/db-save-survey-result.ts
@@ -11,7 +11,7 @@ export class DbSaveSurveyResult implements SaveSurveyResult {
   ) {}
 
   async save (data: SaveSurveyResultModel): Promise<SurveyResultModel> {
-    await this.saveSurveyResultRepository.save(data);
-    return null;
+    const surveyResult = await this.saveSurveyResultRepository.save(data);
+    return surveyResult;
   }
 }
